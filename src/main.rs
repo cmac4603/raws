4bea7dbb7dea6fdda67f6cc68edd6cbcbf16ffb3
Colin MacRae <cmac4603@users.noreply.github.com>
2017-10-15 09:21:00 +0100

    v0.1.1 - removed env::args collecting for now

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 extern crate rusoto_core;
 extern crate rusoto_ec2;
 
-use std::env;
+// use std::env;
 use rusoto_core::default_tls_client;
 use rusoto_core::{DefaultCredentialsProvider, Region};
 use rusoto_ec2::{Ec2, Ec2Client, DescribeInstancesRequest};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    // let args: Vec<String> = env::args().collect();
+    // println!("{:?}", args);
     let provider = DefaultCredentialsProvider::new().unwrap();
     let client = Ec2Client::new(default_tls_client().unwrap(), provider, Region::EuWest1);
     let list_ec2_input: DescribeInstancesRequest = Default::default();
