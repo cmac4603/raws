6e266dd039e2a27d36bf4de160db1fb10e0f437d
Colin MacRae <cmac4603@users.noreply.github.com>
2017-10-17 00:22:00 +0100

    v0.2.0 - use profile name taken from cli

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 extern crate rusoto_core;
 extern crate rusoto_ec2;
 
-// use std::env;
+use std::env;
 use rusoto_core::default_tls_client;
-use rusoto_core::{DefaultCredentialsProvider, Region};
+use rusoto_core::{ProfileProvider, Region};
 use rusoto_ec2::{Ec2, Ec2Client, DescribeInstancesRequest};
 
 fn main() {
-    // let args: Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
     // println!("{:?}", args);
-    let provider = DefaultCredentialsProvider::new().unwrap();
+    let account = args[1].as_ref();
+    let provider = ProfileProvider::with_configuration("/Users/cmacrae/.aws/credentials", account);
     let client = Ec2Client::new(default_tls_client().unwrap(), provider, Region::EuWest1);
     let list_ec2_input: DescribeInstancesRequest = Default::default();
 
@@ -27,7 +28,7 @@ fn main() {
                                     Some(tag_key) => {
                                         if tag_key == "Name" {
                                             println!("{}: {}",
-                                                     tags.value.unwrap(),
+                                                     tags.value.unwrap_or(String::from("No name")),
                                                      ec2_details
                                                          .public_ip_address
                                                          .clone()
